8a7dab0684ff2fc34096b880c8361fa236e83ef8
EPro30 <82483823+EPro30@users.noreply.github.com>
2021-09-13 14:36:47 -0400

    Initial commit

---
+++ src/main/java/exSimpleInterest.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Riya Singh
+ */
+
+import java.util.Scanner;
+public class exSimpleInterest {
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the principal: ");
+        int prncple = sc.nextInt();
+        System.out.println("Enter the rate of Interest: ");
+        double rate = sc.nextDouble();
+        System.out.println("Enter the number of years: ");
+        int time = sc.nextInt();
+
+        double amount = prncple*(1 + ((rate/100)*time));
+        //System.out.printf("After %d years at %f /% rate, the investment will be worth $0.2f.", time, rate, amount);
+        System.out.println("After " + time + " years at " + rate + "% rate, the investment will be worth $" + (int)amount + ".");
+    }
+}
